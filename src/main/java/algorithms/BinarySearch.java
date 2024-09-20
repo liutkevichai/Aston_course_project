1faa2d5c82215e3173953618b6c8c59c4e4cc1a3
Aslan Javasky <71359713+AslanJavasky@users.noreply.github.com>
2024-09-20 21:48:10 +0300

    custom classes+hashcode,equals,toString

--- src/main/java/algorithms/BinarySearch.java
+++ src/main/java/algorithms/BinarySearch.java
@@ -1,20 +1,23 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinarySearch<T extends Comparable<T>> {
-    public int search(T[] arr, T key) {
-        return binarySearch(arr, key, 0, arr.length - 1);
+    public int search(List<T> list, T key) {
+        return binarySearch(list, key, 0, list.size() - 1);
     }
 
-    private int binarySearch(T[] arr, T key, int left, int right) {
+    private int binarySearch(List<T> list, T key, int left, int right) {
         if (left <= right) {
             int mid = left + (right - left) / 2;
-            int cmp = key.compareTo(arr[mid]);
+            int cmp = key.compareTo(list.get(mid));
             if (cmp == 0) {
                 return mid;
             } else if (cmp < 0) {
-                return binarySearch(arr, key, left, mid - 1);
+                return binarySearch(list, key, left, mid - 1);
             } else {
-                return binarySearch(arr, key, mid + 1, right);
+                return binarySearch(list, key, mid + 1, right);
             }
         }
         return -1;
