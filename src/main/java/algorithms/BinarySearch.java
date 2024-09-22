10507e4c531d4f4ba07832eec5d25759087a4dd3
Liutkevich Alisa <liutkevichai@gmail.com>
2024-09-22 12:17:19 +0300

    Changed BinarySearch methods to static.

--- src/main/java/algorithms/BinarySearch.java
+++ src/main/java/algorithms/BinarySearch.java
@@ -1,14 +1,13 @@
 package algorithms;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class BinarySearch<T extends Comparable<T>> {
-    public int search(List<T> list, T key) {
+public class BinarySearch {
+    public static <T extends Comparable<? super T>> int search(List<T> list, T key) {
         return binarySearch(list, key, 0, list.size() - 1);
     }
 
-    private int binarySearch(List<T> list, T key, int left, int right) {
+    private static <T extends Comparable<? super T>> int binarySearch(List<T> list, T key, int left, int right) {
         if (left <= right) {
             int mid = left + (right - left) / 2;
             int cmp = key.compareTo(list.get(mid));
