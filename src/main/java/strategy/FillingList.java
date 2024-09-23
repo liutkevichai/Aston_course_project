96e14e428d24d230179c5302cdbdf3fe27d63ccb
artem <artemkirlanovia@gmail.com>
2024-09-23 18:17:55 +0300

    Add comments

--- src/main/java/strategy/FillingList.java
+++ src/main/java/strategy/FillingList.java
@@ -2,6 +2,9 @@ package strategy;
 
 import java.util.List;
 
+/***
+ * Interface for create and fill object list in certain amount
+ */
 public interface FillingList {
     <T> List fillList(T typeClass, int count);
 }
--- src/main/java/strategy/impl/FillingListFromConsole.java
+++ src/main/java/strategy/impl/FillingListFromConsole.java
@@ -8,7 +8,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/***
+ * Class response for create and fill object list from console
+ */
 public class FillingListFromConsole implements FillingList {
+    /***
+     * Method create and fill object list with type T certain size
+     * This method implements external class for executes to fill
+     * object list in real time
+     * @param typeClass potential class
+     * @param count number element that will consist of object list
+     * @return object list with type T
+     * @param <T> type typed class
+     */
     @Override
     public <T> List fillList(T typeClass, int count) {
         List list = null;
